0ab998ae0a00fc043481358b2ff4d4e9b578bf72
sattimim <43956012+sattimim@users.noreply.github.com>
2019-08-06 23:29:40 -0700

    initial commit

---
+++ src/main/java/com/smsr/sringboot/testapp01/Testapp01Application.java
@@ -0,0 +1,13 @@
+package com.smsr.sringboot.testapp01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testapp01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Testapp01Application.class, args);
+	}
+
+}
